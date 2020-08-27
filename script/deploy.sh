b20bf44affaf504fc9e34c833fbbf73912e59831
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2020-08-27 10:14:13 -0400

    fix type, test on all branches

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,5 @@
 set -e
 
 bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin"
-docker tag zutherb/monolithic-shop:latest $DOCKER_USERNAME/monolithic-shop:latest
+docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
 docker push $DOCKER_LOGIN/monolithic-shop:latest
