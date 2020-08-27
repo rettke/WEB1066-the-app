08f8610d5af54289cf27cfb4d7bcd37d2f8b8296
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2020-08-27 12:31:49 -0400

    redo bash

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin
+bash -c  "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin"
 docker pull zutherb/monolithic-shop:latest
 docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
 docker push $DOCKER_LOGIN/monolithic-shop:latest
